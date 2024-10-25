5e3c61d20eb1a8403bed3b8e4945bc603daef6eb
luciuswilbert <138022429+luciuswilbert@users.noreply.github.com>
2024-10-25 22:13:18 +0800

    Add files via upload

---
+++ Supplier.java
@@ -0,0 +1,50 @@
+package OODJassignment;
+public class Supplier {
+    private int supId;
+    private String supName;
+    private String supEmail;
+    private String supConNum;
+
+    public Supplier() {
+        
+    }
+    
+    public Supplier(int supId, String supName, String supEmail, String supConNum) {
+        this.supId = supId;
+        this.supName = supName;
+        this.supEmail = supEmail;
+        this.supConNum = supConNum;
+    }
+    
+    public String getSupId() {
+        return String.format("Sup" + "%03d", supId);
+    }
+    
+    public void setSupId(int supId) {
+        this.supId = supId;
+    }
+    
+    public String getSupName() {
+        return supName;
+    }
+    
+    public void setSupName(String supName) {
+        this.supName = supName;
+    }
+    
+    public String getSupEmail() {
+        return supEmail;
+    }
+    
+    public void setSupEmail(String supEmail) {
+        this.supEmail = supEmail;
+    }
+    
+    public String getConNum() {
+        return supConNum;
+    }
+    
+    public void setConNum(String supConNum) {
+        this.supConNum = supConNum;
+    }
+}
