cee287d14f272ff4ec07ccb1c5ece3aa22d7cdc3
luciuswilbert <luciuswilbert@gmail.com>
2024-11-11 13:06:51 +0800

    little design

--- src/javaassignment/JavaAssignment.java
+++ src/javaassignment/JavaAssignment.java
@@ -1,20 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package javaassignment;
 
-/**
- *
- * @author Lucius
- */
+import javax.swing.SwingUtilities;
+
 public class JavaAssignment {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                InventoryManager inventoryManager = new InventoryManager();
+                inventoryManager.setVisible(true); // Show the form
+            }
+        });
+        
+        
     }
+    
     
 }
