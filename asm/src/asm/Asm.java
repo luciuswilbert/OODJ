cc1f45f949fa59f2a7712f12d99e9cbe3c9abce2
Samantha Tee Ker Xin <142198252+helloSamTee@users.noreply.github.com>
2024-11-17 00:22:09 +0800

    Update Asm.java

--- asm/src/asm/Asm.java
+++ asm/src/asm/Asm.java
@@ -1,7 +1,5 @@
 package asm;
 
-import asm.purchaseOrder.PurchaseManager_PO;
-import asm.purchaseOrder.purchaseOrder;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.ParseException;
@@ -11,18 +9,20 @@ import java.util.logging.Logger;
 public class Asm {
 
     public static void main(String[] args) {
-//        try {
-//            purchaseOrder.Read();
+        try {
+            purchaseOrder.Read();
 //            purchaseOrder po1 = purchaseOrder.getAll_po().get("po0002");
 //            System.out.println(po1.getSupplier_id());
-//            purchaseOrder po2;
-//            po2 = new purchaseOrder("pr004", "sp004");
+            purchaseOrder po1, po2, po3;
+            po1 = new purchaseOrder("PR0001", "SUP0001", "Sam");
+            po2 = new purchaseOrder("PR0001", "SUP0001", "Sam");
+            po3 = new purchaseOrder("PR0001", "SUP0001", "Sam");
 //            System.out.println(po2.getPr_id());
-//        } catch (ParseException | IOException ex) {
-//            Logger.getLogger(Asm.class.getName()).log(Level.SEVERE, null, ex);
-//        }
+        } catch (ParseException | IOException ex) {
+            Logger.getLogger(Asm.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
-        PurchaseManager_PO p1 = new PurchaseManager_PO();
+        // PurchaseManager_PO p1 = new PurchaseManager_PO();
 
     }
     
